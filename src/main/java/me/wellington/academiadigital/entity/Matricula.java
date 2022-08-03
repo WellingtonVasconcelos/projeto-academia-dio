874bc77ee2d27002ca11c36c388d56f4e10fcec0
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-08-03 16:13:46 -0300

    Anotações da Entidades P1

--- src/main/java/me/wellington/academiadigital/entity/Matricula.java
+++ src/main/java/me/wellington/academiadigital/entity/Matricula.java
@@ -1,11 +1,25 @@
 package me.wellington.academiadigital.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "td_matriculas")
 public class Matricula {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
     private LocalDateTime dataDaMatricula = LocalDateTime.now();
